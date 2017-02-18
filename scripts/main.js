aec5a98d68d2dc5dd6d8c0d9684cad87a97e5dc4
Shelley Melchior <spotty4@gmail.com>
2017-02-18 04:53:59 -0800

    Edits to main.js up to p.123.

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,25 @@ var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
 
-function setDetails() {
+function setDetails(imageUrl, titleText) {
   'use strict';
   var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
-  detailImage.setAttribute('src', 'img/otter3.jpg');
+  detailImage.setAttribute('src', imageUrl);
   var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
-  detailTitle.textContent = 'Elizabeth Warren';
+  detailTitle.textContent = titleText;
+}
+
+function imageFromThumb(thumbnail) {
+  'use strict';
+  return thumbnail.getAttribute('data-image-url');
+}
+
+function titleFromThumb(thumbnail) {
+  'use strict';
+  return thumbnail.getAttribute('data-image-title');
+}
+
+function setDtailsFromThumb(thumbnail) {
+  'use strict';
+  setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
 }
