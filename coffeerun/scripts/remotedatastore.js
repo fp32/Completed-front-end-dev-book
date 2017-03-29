d58a0e3b4da27ac254d280877ac20701f590248c
Shelley Melchior <spotty4@gmail.com>
2017-03-29 16:55:45 -0700

    Completed through Ch. 13 p.269.

--- coffeerun/scripts/remotedatastore.js
+++ coffeerun/scripts/remotedatastore.js
@@ -18,9 +18,17 @@
     });
   };
 
-  RemoteDataStore.prototype.getAll = function () {
+  RemoteDataStore.prototype.getAll = function (cb) {
     $.get(this.serverUrl, function (serverResponse) {
       console.log(serverResponse);
+      cb(serverResponse);
+    });
+  };
+
+  RemoteDataStore.prototype.get = function (key, cb) {
+    $.get(this.serverUrl + '/' + key, function (serverResponse) {
+    console.log(serverResponse);
+    cb(serverResponse);
     });
   };
   
