b47f9fa6394437c0aa4a7c1541f4eaac3004c5e2
Shelley Melchior <spotty4@gmail.com>
2017-03-30 19:51:46 -0700

    Completed through ch. 13.

--- coffeerun/scripts/remotedatastore.js
+++ coffeerun/scripts/remotedatastore.js
@@ -32,6 +32,12 @@
     });
   };
   
+  RemoteDataStore.prototype.remove = function (key) {
+    $.ajax(this.serverUrl + '/' + key, {
+    type: 'DELETE'
+    });
+  };
+  
   App.RemoteDataStore = RemoteDataStore;
   window.App = App;
 
